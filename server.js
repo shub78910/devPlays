ae377bcbbcfad15fe09af4f63c8aa1fcc54bebf5
shubhamNoustek <shubham.hirakki@noustek.com>
2022-01-15 21:12:44 +0530

    changed file names for deployment

--- server.js
+++ server.js
@@ -12,8 +12,6 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
-
-// 👇️ "/home/john/Desktop/javascript"
 const __dirname = path.dirname(__filename);
 
 const app = express();
